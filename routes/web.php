54f25c2f37e85df7aeaf57174832da0b3acbc02f
level7up <abdoleemos200@gmail.com>
2022-11-26 13:58:49 +0200

    :fire:

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Level7up\Cms\Http\Controllers\BlogController;
 
-Route::get('blogs', [BlogController::class, 'index'])->name('blogs.index');
+// use Level7up\Cms\Http\Controllers\BlogController;
+
+// Route::get('blogs', [BlogController::class, 'index'])->name('blogs.index');
